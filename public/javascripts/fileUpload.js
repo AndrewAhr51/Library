00d3001e0037d7901ce678c6ce492d0994513a87
Andrew Ahr <andrew.ahr51@gmail.com>
2020-04-01 08:06:47 -0600

    Fixing css

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,13 +1,29 @@
-FilePond.registerPlugin(
-    FilePondPluginImagePreview,
-    FilePondPluginImageResize,
-    FilePondPluginFileEncode,
-)
+const rootStyle =window.getComputedStyle
+(documen.documentElement)
 
-FilePond.setOptions({
-    stylePanelAspectRatio: 150 / 100,
-    imageResizeTargetWidth: 100,
-    imageResizeTargetHeight: 150
-})
+if (rootStyles.getPropertyValue('--book-cover-width-large') !=null && rootStyles.getPropertyValue('--book-cover-width-large') !='') {
+    ready();
+} else{
+    document.getElementById('main-css').addEventListener('load', ready);
+}
 
-FilePond.parse(document.body);
+function ready() {
+    const coverWidth = parseFloat( rootStyle.getPropertyValue('--book-cover-width-large'));
+    const coverAspectRatio = parseFloat(rootStyle.getPropertyValue('--book-cover-aspect-ration'));
+    const coverHeight=  parseFloat(coverWidth/coverAspectRation);
+
+    FilePond.registerPlugin(
+        FilePondPluginImagePreview,
+        FilePondPluginImageResize,
+        FilePondPluginFileEncode,
+    )
+    
+    FilePond.setOptions({
+        stylePanelAspectRatio: 1 / coverAspectRatio,
+        imageResizeTargetWidth: coverWidth,
+        imageResizeTargetHeight: coverHeight
+    })
+    
+    FilePond.parse(document.body);
+}
+
